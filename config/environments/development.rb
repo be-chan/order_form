286b9f44e61a81b2cc686e489e013a01fd47ce69
be-chan <70454487+be-chan@users.noreply.github.com>
2023-12-10 02:04:05 +0900

    better_errorsを使えるようにgemの追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # dockerでbetter_errorsを使うために記述
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
 end
